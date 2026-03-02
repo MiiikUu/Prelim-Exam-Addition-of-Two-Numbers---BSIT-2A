be4c385c0741edad03dd2ed5c16030a19a28529f
MiiikUu <aspaciopaul438@gmail.com>
2026-03-02 16:12:28 +0800

    Prelim Exam

---
+++ AdditionofTwoNumbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdditionofTwoNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter second number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            int result = num1 + num2;
+
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+        }
+    }
+}
